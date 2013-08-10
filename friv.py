6ae81f7b2b40ff9bee510699591d021462f2b6ab
masagojira <masa.gojira@gmail.com>
2013-08-10 14:12:12 +0400

    Add mouse wheel image switching

--- friv.py
+++ friv.py
@@ -7,7 +7,7 @@ pygtk.require('2.0')
 import gtk
 import sys
 import os.path
-import glob
+import re
 
 
 class friv:
@@ -15,15 +15,20 @@ class friv:
     window = object()
     working_dir = "~"
     image_files = []
-    current_image_index = 0
+    image_index = 0
     image = gtk.Image()
     preloaded_image = gtk.Image()
 
-    #fixme: change numbers to actual screen size
-    screen_width, screen_height = 1920, 1080
+    screen_width = gtk.gdk.screen_width()
+    screen_height = gtk.gdk.screen_height()
 
-    def next_image(self):
-        pass
+    def next_image_pixbuf(self):
+        pixbuf = self.load_pixbuf(self.working_dir + '/' + self.image_files[self.image_index])
+        return pixbuf
+
+    def image_files_in_dir(self, dir_path):
+        image_file_list = [f for f in os.listdir(dir_path) if re.search(r'.*\.(jpg|png|gif)$', f)]
+        return sorted(image_file_list, reverse=True)
 
     def close_application(self, widget, event, data=None):
         gtk.main_quit()
@@ -32,8 +37,8 @@ class friv:
     def toggle_fullscreen(self):
         pass
 
-    def load_image(self, filename):
-        self.image = gtk.Image()
+    def load_pixbuf(self, filename):
+
         pixbuf = gtk.gdk.pixbuf_new_from_file(filename)
 
         width = pixbuf.get_width()
@@ -48,22 +53,21 @@ class friv:
             height = width / aspect
 
         pixbuf = pixbuf.scale_simple(int(width), int(height), gtk.gdk.INTERP_BILINEAR)
-        self.image.set_from_pixbuf(pixbuf)
-        return self.image
+        return pixbuf
 
     def show_error(self):
         pass
 
-    def mouse_scroll(self, widget, event):
-        if event.type == 4:
-            # scroll up
-            pass
-        elif event.type == 5:
-            # scroll down
-            pass
+    def mouse_scroll(self, widget, event, image):
 
-    def get_image_files_from_dir(self):
-        self.image_files.extend(glob(self.working_dir+'\.jpg'))
+        if event.direction == gtk.gdk.SCROLL_UP:
+            if self.image_index < len(self.image_files):
+                self.image_index += 1
+        elif event.direction == gtk.gdk.SCROLL_DOWN:
+            if self.image_index > 0:
+                self.image_index -= 1
+
+        image.set_from_pixbuf(self.next_image_pixbuf())
 
     def __init__(self):
 
@@ -89,20 +93,27 @@ class friv:
                                 gtk.main_quit)
         self.window.add_accel_group(accelgroup)
 
-        #Mouse scroll
-        self.window.add_events(gtk.gdk.SCROLL_MASK)
-        self.window.connect("scroll-event", self.mouse_scroll)
-
         # Loading first file
         try:
             start_file_path = str(sys.argv[1])
-            print(start_file_path)
+
             self.working_dir, filename = os.path.split(start_file_path)
-            self.image = self.load_image(start_file_path)
+            # print(self.working_dir)
+            self.image_files.extend(self.image_files_in_dir(self.working_dir))
+            # print(self.image_files)
+            self.image_index = self.image_files.index(filename)
+            # print(str(len(self.image_files)) + ' ' + str(self.current_image_index))
+            self.image = gtk.Image()
+            self.image.set_from_pixbuf(self.load_pixbuf(start_file_path))
             self.image.show()
             hbox.add(self.image)
         except IndexError:
             pass
+
+        #Mouse scroll
+        self.window.add_events(gtk.gdk.SCROLL_MASK)
+        # self.image included for update image
+        self.window.connect("scroll-event", self.mouse_scroll, self.image)
 
         self.window.show()
 
